b19f222611199b46a17d6ca980bb3914f0887660
Christopher A. Jones <cjones7@ltu.edu>
2025-11-14 17:13:12 -0500

    Fixed Music

--- Assets/Scripts/CannonBallGoRightScript.cs
+++ Assets/Scripts/CannonBallGoRightScript.cs
@@ -6,6 +6,6 @@ public class CannonBallGoRightScript : MonoBehaviour
 {
     void Update()
     {
-        transform.position += new Vector3(0.025f, 0f, 0f);
+        transform.position += new Vector3(0.05f, 0f, 0f);
     }
 }
