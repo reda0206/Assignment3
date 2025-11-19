c9b59a96b103d62ce6dd1873ea4af0da58a4c62c
Christopher A. Jones <cjones7@ltu.edu>
2025-11-19 14:15:01 -0500

    Fixed Cannonballs still moving while paused

--- Assets/Scripts/CannonBallGoRightScript.cs
+++ Assets/Scripts/CannonBallGoRightScript.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class CannonBallGoRightScript : MonoBehaviour
 {
+    public float speed = 5f;
     void Update()
     {
-        transform.position += new Vector3(0.05f, 0f, 0f);
+        if (GameManager.Instance != null && GameManager.Instance.isPaused) return;
+
+        if (Mathf.Approximately(Time.timeScale, 0f)) return;
+
+        transform.position += Vector3.right * speed * Time.deltaTime;
     }
 }
